9bcfb5f55e7cbb864dfc442941ed6136ea48946c
dwzhao <dwzhao@corp.netease.com>
2020-11-20 19:13:48 +0800

    增加汉字注释

--- dwzhaopt/echo.py
+++ dwzhaopt/echo.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
-# Usage: ./build.py group/project
+# -*- coding:utf-8 -*-
+
+# 消息回声脚本
 
 import sys
 
 
 def echo():
+    """
+    消息回声，无论怎样的请求都使用print输出
+
+    :return: None
+    """
     if len(sys.argv) < 2:
         print 'Usage: {} msg'.format(__file__)
         sys.exit(1)
